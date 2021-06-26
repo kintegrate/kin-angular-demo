31dcbc9694c865a3d7e67c0de876b51879d3e19a
Bram Borggreve <borggreve@gmail.com>
2021-06-26 05:42:23 -0500

    @kin-sdk/client in Angular 12.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,52 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { createWallet, KinClient, KinTest, Wallet } from '@kin-sdk/client'
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
+    <button (click)="go()">Go</button>
+    <pre>wallet {{ wallet | json }}</pre>
+    <pre>balances: {{ balances | json }}</pre>
+    <pre>destination: {{ destination | json }}</pre>
     <router-outlet></router-outlet>
   `,
-  styles: []
 })
 export class AppComponent {
-  title = 'kin-angular-demo';
+  readonly kin = new KinClient(KinTest)
+  balances
+  wallet
+  destination
+
+  async go() {
+    this.wallet = createWallet('create')
+    await this.kin.createAccount(this.wallet.secret).then(() => {
+      console.log('Wallet Created')
+    })
+    await this.checkBalance()
+    await this.airDrop()
+    await this.checkBalance()
+    await this.kin.submitPayment({
+      secret: this.wallet.secret,
+      tokenAccount: this.wallet.publicKey,
+      amount: '50000',
+      destination: 'Don8L4DTVrUrRAcVTsFoCRqei5Mokde3CV3K9Ut4nAGZ',
+    })
+    await this.checkBalance()
+  }
+
+  async airDrop() {
+    await this.kin.requestAirdrop(this.wallet.publicKey, '50000').then((res) => {
+      console.log('Airdrop', res)
+    })
+  }
+
+  async checkBalance() {
+    await this.kin.getBalances(this.wallet.publicKey).then((balances) => {
+      this.balances = balances
+    })
+
+    await this.kin.getBalances('Don8L4DTVrUrRAcVTsFoCRqei5Mokde3CV3K9Ut4nAGZ').then((balances) => {
+      this.destination = balances
+    })
+  }
 }
